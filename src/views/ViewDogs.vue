0c07df8d55f8fca09b57e351df8934d2789e2084
Ugbah Isioma <ugbahisioma@gmail.com>
2023-02-09 16:09:56 +0100

    feat: added api fetch logic to render list of dogs

--- src/views/ViewDogs.vue
+++ src/views/ViewDogs.vue
@@ -1,9 +1,27 @@
 <script lang="ts" setup>
+import { useQuery } from "@tanstack/vue-query";
 import DogCard from "../components/DogCard.vue";
+import axiosInstance from "../lib/axioinstance";
+
+const getRandomDogs = async () => {
+  const res = await axiosInstance.get("/breeds/image/random/100");
+  return res.data;
+};
+
+const { data, isLoading } = useQuery({
+  queryKey: ["random-dogs"],
+  queryFn: getRandomDogs,
+  select: (data) => data.message,
+});
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-    <DogCard image="/dog.jpg" breed="Husky" />
+  <div v-if="isLoading">
+    <p>...loading</p>
+  </div>
+  <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <template v-for="el in data">
+      <DogCard :image="el" breed="Husky" />
+    </template>
   </div>
 </template>

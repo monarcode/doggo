0c46124c013a7a39082772b9e7b15989545e0ee8
Ugbah Isioma <ugbahisioma@gmail.com>
2023-02-09 16:37:27 +0100

    fix: updated breed detection logic

--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import { computed } from "vue";
+
 type DogCardProps = {
   image: string;
   breed: string;
 };
 const props = defineProps<DogCardProps>();
+
+const getBreedName = (str: string) => {
+  return str.split("/")[4];
+};
+const breedname = computed(() => {
+  return getBreedName(props.image);
+});
 </script>
 
 <template>
@@ -15,10 +24,12 @@ const props = defineProps<DogCardProps>();
       lazy
     />
     <div class="dog-content flex flex-col justify-evenly items-center">
-      <h2 class="text-3xl capitalize">{{ props.breed }}</h2>
+      <h2 class="text-2xl capitalize whitespace-nowrap">
+        {{ breedname }}
+      </h2>
       <router-link
         class="bg-accent text-white py-2 px-4 text-sm rounded-full"
-        :to="props.breed"
+        :to="breedname"
         >See more</router-link
       >
     </div>

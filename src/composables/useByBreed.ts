895dd3118219dee00e94fe052f386ce1588efb14
Ugbah Isioma <ugbahisioma@gmail.com>
2023-02-09 19:15:19 +0100

    fix: fixed sub-breed query bug

--- src/composables/useByBreed.ts
+++ src/composables/useByBreed.ts
@@ -2,15 +2,29 @@ import { useQuery } from "@tanstack/vue-query";
 import axiosInstance from "../lib/axioinstance";
 
 const useFetchByBreed = (breed: string) => {
-  return useQuery({
-    queryKey: ["fetch-dog-breed", breed],
-    queryFn: async () => {
-      return axiosInstance
-        .get(`/breed/${breed}/images`)
-        .then((res) => res.data);
-    },
-    select: (data) => data.message,
-  });
+  if (!breed.includes("-")) {
+    return useQuery({
+      queryKey: ["fetch-dog-breed", breed],
+      queryFn: async () => {
+        return axiosInstance
+          .get(`/breed/${breed}/images`)
+          .then((res) => res.data);
+      },
+      select: (data) => data.message,
+    });
+  } else {
+    const brArr = breed.split("-").join("/");
+
+    return useQuery({
+      queryKey: ["fetch-dog-breed", breed],
+      queryFn: async () => {
+        return axiosInstance
+          .get(`/breed/${brArr}/images`)
+          .then((res) => res.data);
+      },
+      select: (data) => data.message,
+    });
+  }
 };
 
 export default useFetchByBreed;
